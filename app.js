a9f94f6f3195aa34ec4812dfb395d09eceb596fc
Mahmoud <mahmodadel@gmail.com>
2025-04-25 18:42:10 +0300

    add helmet

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
+import helmet from "helmet";
 
 import { PORT } from "./config/env.js";
 import errorHandler from "./middlewares/err.middleware.js";
@@ -14,6 +15,7 @@ import commentRoute from "./routes/comment.routes.js";
 const app = express();
 
 app.use(cors());
+app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan("dev"));
